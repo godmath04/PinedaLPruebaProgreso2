1196249cb8fe4017be042d124ba7651b77602fb0
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-06-03 07:16:10 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PinedaLPruebaProgreso2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
